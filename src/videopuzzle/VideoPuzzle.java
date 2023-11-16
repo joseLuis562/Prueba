25715aae9435180c889598bd20774547e8f25e1f
joseluis <jose.aguilartun@cbtis72.edu.mx>
2023-11-16 16:01:36 -0500

    mi commit

---
+++ src/videopuzzle/VideoPuzzle.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videopuzzle;
+
+
+public class VideoPuzzle {
+
+    public static void main(String[] args) {
+        ventanaPuzzle ventana = new ventanaPuzzle();
+        ventana.setVisible(true);
+    }
+}
